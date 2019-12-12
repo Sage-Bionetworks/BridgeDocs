969bd1e0325075cc350ff557e33c493edb472f2b
Alx Dark <alx.dark@sagebase.org>
2019-12-11 16:26:29 -0800

    Updating pre-loader.

--- scripts/shared-module-widget.js
+++ scripts/shared-module-widget.js
@@ -5,50 +5,51 @@ ldg.className="loading";
 ldg.textContent = "Loading modules...";
 root.parentNode.insertBefore(ldg, root);
 function createCORSRequest(method, url) {
-    var xhr = new XMLHttpRequest();
-    if ("withCredentials" in xhr) {
-        xhr.open(method, url, true);
-    } else if (typeof XDomainRequest != "undefined") {
-        xhr = new XDomainRequest();
-        xhr.open(method, url);
-    } else {
-        xhr = null;
-    }
-    return xhr;
+  var xhr = new XMLHttpRequest();
+  if ("withCredentials" in xhr) {
+    xhr.open(method, url, true);
+  } else if (typeof XDomainRequest != "undefined") {
+    xhr = new XDomainRequest();
+    xhr.open(method, url);
+  } else {
+    xhr = null;
+  }
+  return xhr;
 }
 function displayItems(items) {
     if (items.length === 0) {
-        var div = document.createElement("div");
-        div.setAttribute('data-bridge-sm',true);
+      var div = document.createElement("div");
+      div.setAttribute('data-bridge-sm',true);
 
-        var p = document.createElement("p");
-        p.innerText = "There are no shared modules at this time.";
-        root.parentNode.insertBefore(p, root);
+      var p = document.createElement("p");
+      p.innerText = "There are no shared modules at this time.";
+      root.parentNode.insertBefore(p, root);
     }
     items.forEach(function(item) {
-        var div = document.createElement("div");
-        div.setAttribute('data-bridge-sm',true);
-        var hdr = document.createElement("h3");
-        hdr.innerText = item.name;
-        var body = document.createElement("section");
-        body.innerHTML = (item.notes) ? item.notes : "<p class='no-notes'><em>No information available.</em></p>";
-        div.appendChild(hdr);
-        div.appendChild(body);
-        root.parentNode.insertBefore(div, root);
+      console.log(item);
+      var div = document.createElement("div");
+      div.setAttribute('data-bridge-sm',true);
+      var hdr = document.createElement("h3");
+      hdr.innerText = item.name;
+      var body = document.createElement("section");
+      body.innerHTML = (item.notes) ? item.notes : "<p class='no-notes'><em>No information available.</em></p>";
+      div.appendChild(hdr);
+      div.appendChild(body);
+      root.parentNode.insertBefore(div, root);
     });
     root.parentNode.removeChild(ldg);
 }
-var url = 'https://webservices.sagebridge.org/v3/sharedmodules/metadata?mostrecent=true&published=true&cb=' + new Date().getTime();
+var url = 'https://ws.sagebridge.org/v3/sharedmodules/metadata?mostrecent=true&published=true&cb=' + new Date().getTime();
 var request = createCORSRequest('GET', url);
 if (request) {
-    request.setRequestHeader('Content-Type', 'application/json');
-    request.send();
-    request.onreadystatechange = function() {
-        if(request.readyState === 4 && request.status === 200) {
-            // TODO: sort entries, then possibly reverse it given insertion order
-            var object = JSON.parse(request.responseText);
-            object.items.sort(function(a,b){return a.name.localeCompare(b.name);});
-            displayItems(object.items);
-        }
-    };
+  request.setRequestHeader('Content-Type', 'application/json');
+  request.send();
+  request.onreadystatechange = function() {
+    if(request.readyState === 4 && request.status === 200) {
+      // TODO: sort entries, then possibly reverse it given insertion order
+      var object = JSON.parse(request.responseText);
+      object.items.sort(function(a,b){return a.name.localeCompare(b.name);});
+      displayItems(object.items);
+    }
+  };
 }
