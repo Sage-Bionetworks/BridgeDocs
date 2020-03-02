4758ed05f3f7c93ec1104d1574f8b1dc65959586
Alx Dark <alx.dark@sagebase.org>
2020-03-02 10:39:04 -0800

    Update diagram

--- scripts/main.js
+++ scripts/main.js
@@ -1,33 +1,34 @@
-// Activate the current navigation link, indicate external links
-var items = document.body.querySelectorAll("a");
-for (var i=0; i < items.length; i++) {
-    var item = items[i];
-    var href = item.getAttribute('data-filepath') || item.getAttribute('href');
-    if (!item.hasAttribute('no-ext')) {
-        if (/http[s]?\:/.test(href)) {
-            iconify(item, "external");
-            item.target = "_blank";
-            item.setAttribute('rel', 'noopener');
-        } else if (/mailto\:/.test(href)) {
-            iconify(item, "mail outline");
-        }
-    }
-    if (document.location.pathname.indexOf(href) > -1) {
-        item.classList.add("active");
-        $(item).closest('.content').addClass('active').prev('.title').addClass('active');
-        $("#acc").css({opacity: 1});
-    }
-}
 // Some "meta" pages aren't in the navigation, so in that case, open the general
 // section so some navigation is showing. Flag these articles with nav: nav in the 
 // meta YML of the article.
 if (document.body.hasAttribute('nav')) {
-    $("nav div:nth-child(2)").addClass('active').prev('.title').addClass('active');
+    $("#acc").css({opacity: 1}).find("div:nth-child(2)")
+      .addClass('active').prev('.title').addClass('active');
+} else {
+    // Activate the current navigation link, indicate external links
+    var items = document.body.querySelectorAll("a");
+    for (var i=0; i < items.length; i++) {
+        var item = items[i];
+        var href = item.getAttribute('data-filepath') || item.getAttribute('href');
+        if (!item.hasAttribute('no-ext')) {
+            if (/http[s]?\:/.test(href)) {
+                iconify(item, "external");
+                item.target = "_blank";
+                item.setAttribute('rel', 'noopener');
+            } else if (/mailto\:/.test(href)) {
+                iconify(item, "mail outline");
+            }
+        }
+        if (document.location.pathname.indexOf(href) > -1) {
+            item.classList.add("active");
+            $(item).closest('.content').addClass('active').prev('.title').addClass('active');
+            $("#acc").css({opacity: 1});
+        }
+    }
 }
 var items = document.body.querySelectorAll("table");
 for (var i=0; i < items.length; i++) {
     var item = items[i];
-
     item.className = "ui compact table";
 }
 function iconify(item, iconName) {
